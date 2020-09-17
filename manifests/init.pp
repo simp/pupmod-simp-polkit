813b1e182cadc8336fc9943c426f3e849ad877f3
Trevor Vaughan <tvaughan@onyxpoint.com>
2020-09-17 11:24:22 -0400

    Make entire module inert

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,23 +15,33 @@
 # @param package_ensure
 #   The ensure status of packages
 #
+# @param warn_on_unsupported_os
+#   Warn if the module is trying to be used on an unsupported OS
+#
+#   * The module will not fail on an unsupported OS but also will not perform
+#     any action
+#
 # @author https://github.com/simp/pupmod-simp-polkit/graphs/contributors
 #
 class polkit (
-  Boolean               $manage_polkit_user = true,
-  Polkit::PackageEnsure $package_ensure     = simplib::lookup('simp_options::package_ensure', { 'default_value' => 'installed' })
+  Boolean               $manage_polkit_user     = true,
+  Polkit::PackageEnsure $package_ensure         = simplib::lookup('simp_options::package_ensure', { 'default_value' => 'installed' }),
+  Boolean               $warn_on_unsupported_os = true
 ){
-  simplib::assert_metadata($module_name)
+  if simplib::module_metadata::os_supported( load_module_metadata($module_name), { 'release_match' => 'major' }) {
+    include polkit::install
+    include polkit::service
 
-  include polkit::install
-  include polkit::service
+    Class['polkit::install'] ~> Class['polkit::service']
 
-  Class['polkit::install'] ~> Class['polkit::service']
+    if $manage_polkit_user {
+      include polkit::user
 
-  if $manage_polkit_user {
-    include polkit::user
-
-    Class['polkit::install'] -> Class['polkit::user']
-    Class['polkit::user'] ~> Class['polkit::service']
+      Class['polkit::install'] -> Class['polkit::user']
+      Class['polkit::user'] ~> Class['polkit::service']
+    }
+  }
+  elsif $warn_on_unsupported_os {
+    warning("${facts['os']['name']} ${facts['os']['release']['full']} is not supported by ${module_name}. To silence this warning, set ${module_name}::warn_on_unsupported_os to 'false'")
   }
 }
