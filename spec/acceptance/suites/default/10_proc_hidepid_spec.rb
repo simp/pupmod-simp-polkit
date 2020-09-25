972b8f2254d8a04a577e5e6bdca46e3fa9e394bf
jeannegreulich <jeanne.greulich@onyxpoint.com>
2020-09-25 18:59:27 -0400

    (SIMP-8405) Polkit Acceptance Tests (#42)
    (SIMP-8405) simp-polkit acceptance tests not run in GitLab
    
    * Fixed acceptance tests
      * Fixed ordering problem
      * Changed service command to systemctl as this module works on EL7+
      * Only validate polkit bug is fixed when proc is setup with hidepid and a group
    * Added acceptance tests to GitLab
    
    SIMP-8405 #close

--- spec/acceptance/suites/default/10_proc_hidepid_spec.rb
+++ spec/acceptance/suites/default/10_proc_hidepid_spec.rb
@@ -5,43 +5,46 @@ require 'spec_helper_acceptance'
 test_name 'polkit with /proc hidepid=2'
 
 describe 'polkit with /proc hidepid=2' do
-  hosts.each do |_host|
+  hosts.each do |host|
     let(:manifest) do
       'include polkit'
     end
+    context "Set hidepid but not gid on /proc on #{host}" do
+      it 'remounts /proc with hidepid=2' do
+        # if the gid is set then it does not get cleared unless rebooted
+        # The fstab should not have gid or hidepid set at this point.
+        host.reboot
+        on(host, 'mount -o remount,hidepid=2 /proc')
+      end
 
-    hosts.each do |host|
-      context "on #{host}" do
-        it 'remounts /proc with hidepid=2' do
-          on(host, 'mount -o remount,hidepid=2 /proc')
-        end
+      it 'shows a notification message but does not restart the service' do
+        output = apply_manifest_on(host, manifest, :accept_all_exit_codes => true).output
+        expect(output).to match(/hidepid warning/)
+        expect(output).not_to match(/Service/)
+      end
 
-        it 'shows a notification message but does not restart the service' do
-          output = apply_manifest_on(host, manifest, :accept_all_exit_codes => true).output
+    end
 
-          expect(output).to match(/hidepid warning/)
-          expect(output).not_to match(/Service/)
-        end
+    context "Set hidepid and gid on /proc on #{host}" do
+      # DO NOT DO THIS IN PRODUCTION - JUST FOR TESTING
+      it 'remounts /proc with hidepid=2 and gid=100' do
+        on(host, 'mount -o remount,hidepid=2,gid=100 /proc')
+      end
 
-        # DO NOT DO THIS IN PRODUCTION - JUST FOR TESTING
-        it 'remounts /proc with hidepid=2 and gid=100' do
-          on(host, 'mount -o remount,hidepid=2,gid=100 /proc')
-        end
+      it 'applies with no errors and does not show a notification message' do
+        output = apply_manifest_on(host, manifest).output
 
-        it 'applies with no errors and does not show a notification message' do
-          output = apply_manifest_on(host, manifest).output
+        expect(output).not_to match(/hidepid warning/)
+      end
 
-          expect(output).not_to match(/hidepid warning/)
-        end
+      it 'is idempotent' do
+        apply_manifest_on(host, manifest, catch_changes: true)
+      end
 
-        it 'is idempotent' do
-          apply_manifest_on(host, manifest, catch_changes: true)
-        end
-
-        it 'does not show pkttyagent warnings when running service restarts' do
-          expect(on(host, 'service foo restart', :accept_all_exit_codes => true).output)
-            .not_to match(%r{pkttyagent.+WARNING})
-        end
+      # See https://simp-project.atlassian.net/browse/SIMP-8228 for the issue this tests.
+      it 'does not show pkttyagent warnings when running service restarts' do
+        expect(on(host, 'systemctl restart foo', :accept_all_exit_codes => true).output)
+          .not_to match(%r{pkttyagent.+WARNING})
       end
     end
   end
